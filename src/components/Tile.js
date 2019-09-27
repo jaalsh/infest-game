7fb30fc8ed368595578c859631ca2e8e6f7e1b95
Jamie Sharpe <jamie.sharpe@waterstons.com>
2019-09-27 11:29:02 +0100

    add tile component

---
+++ src/components/Tile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const dimension = '64px'
+
+const Tile = (imageUrl) => {
+    return (
+        <img src={imageUrl} width={dimension} height={dimension} />
+    );
+  };
+
+  export default Tile;
