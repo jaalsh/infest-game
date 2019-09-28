da9a511e8bb96c0b69ead6b1b8f9b07fe5a5280d
Jamie Sharpe <jamie.sharpe@waterstons.com>
2019-09-28 10:33:49 +0100

    objects

--- src/components/tiles/ConveyorTile.js
+++ src/components/tiles/ConveyorTile.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import Tile from './Tile'
-import ConveyorBeltObject from '../objects/ConveyorBeltObject'
 
 const ConveyorTile = ({ config }) => {
-    const renderObject = () => {
-        return 'objectImageName' in config ? <ConveyorBeltObject objectImageName={config.objectImageName}/> : null;
-    }
-
     return (
-        <>
-            <Tile imageName={`conveyor_${config.direction}`}/>
-            {renderObject()}
-        </>
+        <Tile imageName={`conveyor_${config.direction}`}/>
     );
   };
 
