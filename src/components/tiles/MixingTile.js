928f2659e31e4525c50bbe1f4629582d5e1e96bc
Jamie Sharpe <jamie.sharpe@waterstons.com>
2019-09-28 13:33:43 +0100

    add titles for all tiles

--- src/components/tiles/MixingTile.js
+++ src/components/tiles/MixingTile.js
@@ -3,7 +3,7 @@ import Tile from './Tile'
 
 const MixingTile = ({ config, objects }) => {
     return (
-        <Tile imageName='mixing' objectImageNames={objects} config={config}/>
+        <Tile imageName='mixing' objectImageNames={objects} config={config} />
     );
   };
 
--- src/components/tiles/Tile.js
+++ src/components/tiles/Tile.js
@@ -3,13 +3,12 @@ import GameObject from '../objects/GameObject';
 
 const dimension = '64px'
 
-
-const Tile = ({ imageName, objectImageNames, instructions }) => {
+const Tile = ({ imageName, objectImageNames, config }) => {
     const imageSource = `${process.env.PUBLIC_URL}/assets/images/tiles/${imageName}.png`
 
     return (
         <div style={{ position: 'relative' }}>
-            <img src={imageSource} width={dimension} height={dimension} alt="base_tile" title={instructions ? instructions.map(i => JSON.stringify(i)).join("\r\n") : "no instructions"} />
+            <img src={imageSource} width={dimension} height={dimension} alt="base_tile" title={config.instructions ? config.instructions.map(i => JSON.stringify(i)).join("\r\n") : "no instructions"} />
             {objectImageNames && <GameObject style={{position: 'absolute', left: 0}} objectImageName={objectImageNames[0]} width={dimension} height={dimension}  />}
         </div>
     );
